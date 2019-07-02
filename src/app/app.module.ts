3fb43db54f1d96fd004cca9b0afd832cb61a8710
nadia <nadia.mohamed.taha166@gmail.com>
2019-07-02 12:05:20 +0200

    ,,

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { QrCodeAllModule } from 'ngx-qrcode-all';
 
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
@@ -48,6 +49,8 @@ export function createTranslateLoader(http: HttpClient) {
   ],
   imports: [
     BrowserModule,
+    QrCodeAllModule,
+
     BrowserAnimationsModule,
     RouterModule.forRoot(AppRoutes),
     FormsModule,
